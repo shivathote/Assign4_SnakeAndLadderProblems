fabbb9ba3904f16b44797dbdc5c2479f53a7fcf0
Shiva Panditrao Thote <shiva.thote@yahoo.in>
2021-10-07 04:36:51 +0530

    Added:UC1_StartPositionZero.java

---
+++ UC1_StartPositionZero.java
@@ -0,0 +1,10 @@
+package com.Assign4.SnakeAndLadder.BridgeLabz;
+
+public class UC1_StartPositionZero {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        int startPosition = 0;
+        System.out.println("Player starting position  : " + startPosition);
+    }
+}
